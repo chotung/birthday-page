7de3c6b410fc763bacc71db67687a33ed32a800c
chotung <choelestung@gmail.com>
2018-10-31 19:30:50 -0400

    Initial commit

---
+++ happy-birthday/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import './App.css'
+
+const App = () => {
+
+  
+  return (
+    <div className="welcome">
+      <h1>
+        hello
+      </h1>
+      <br>
+      </br>
+      <h2>
+        world
+      </h2>
+    </div>
+  )
+}
+
+export default App
+
+// add fireworks or something
+// might change the background
+// time render the transition
